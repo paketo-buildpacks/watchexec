a0d2d3673a6f1f9ebe03c94903257d2f577a8e62
Daniel Mikusa <dan@mikusa.com>
2026-09-19 13:58:49 -0400

    GitHub config (#291)
    * Switch from pipeline-builder to github-config implementation workflows
    
    * Lint fixes
    
    Signed-off-by: Daniel Mikusa <dan@mikusa.com>
    
    * Switch to using libdependency
    
    Signed-off-by: Daniel Mikusa <dan@mikusa.com>
    
    ---------
    
    Signed-off-by: Daniel Mikusa <dan@mikusa.com>

--- watchexec/watchexec.go
+++ watchexec/watchexec.go
@@ -32,8 +32,8 @@ type Watchexec struct {
 	Logger           bard.Logger
 }
 
-func NewWatchexec(dependency libpak.BuildpackDependency, cache libpak.DependencyCache) (Watchexec, libcnb.BOMEntry) {
-	contributor, entry := libpak.NewDependencyLayer(dependency, cache, libcnb.LayerTypes{
+func NewWatchexec(dependency libpak.BuildpackDependency, cache libpak.DependencyCache) (Watchexec, libcnb.BOMEntry) { //nolint:staticcheck // hold off on the BOM migration for now
+	contributor, entry := libpak.NewDependencyLayer(dependency, cache, libcnb.LayerTypes{ //nolint:staticcheck // hold off on the BOM migration for now
 		Launch: true,
 	})
 	return Watchexec{LayerContributor: contributor}, entry
@@ -44,7 +44,7 @@ func (w Watchexec) Contribute(layer libcnb.Layer) (libcnb.Layer, error) {
 
 	return w.LayerContributor.Contribute(layer, func(artifact *os.File) (libcnb.Layer, error) {
 		w.Logger.Bodyf("Expanding to %s", layer.Path)
-		if err := crush.ExtractTarXz(artifact, layer.Path, 1); err != nil {
+		if err := crush.Extract(artifact, layer.Path, 1); err != nil {
 			return libcnb.Layer{}, fmt.Errorf("unable to expand Watchexec\n%w", err)
 		}
 
